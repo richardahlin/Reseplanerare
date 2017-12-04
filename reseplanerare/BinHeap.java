0dd6584b94b34fe95cf9172ea6b748aedec76121
AxelArkheden <axel.arkheden@gmail.com>
2017-12-04 11:15:18 +0100

    Updated binHeap

--- reseplanerare/BinHeap.java
+++ reseplanerare/BinHeap.java
@@ -9,7 +9,8 @@ public class BinHeap<E> implements PrioQueue<E>{
     }
 
     void add(E e){
-
+        queue.add(e);
+        bubbleUp(queue.size() - 1);
     }
 
     E peek(){
@@ -17,14 +18,72 @@ public class BinHeap<E> implements PrioQueue<E>{
     }
 
     E poll(){
+        if(queue.get(0) == null){
+            return null;
+        }
 
+        E returnElement = queue.get(0);
+        queue.remove(0);
+
+        if(queue.size() == 0){
+            return returnElement;
+        }
+
+        E swapElement = queue.get(queue.size() - 1);
+        queue.add(0,swapElement);
+        queue.remove(queue.size() - 1);
+
+        bubbleDown(0);
+
+        return returnElement;
     }
 
     void remove(E e){
+        int equalsIndex;
+        int i = 0;
 
+        for(E element : queue){
+            if(element == e){
+                equalsIndex = i;
+                break;
+            }
+            i++;
+        }
+
+        if(equalsIndex == null){
+            return;
+        }
+
+        queue.remove(equalsIndex);
+
+        if(queue.size() != 0){
+            queue.add(equalsIndex,queue.get(queue.size() - 1));
+            queue.remove(queue.size() - 1);
+            bubbleUpOrDown(equalsIndex);
+        }
     }
 
     void remove(int index){
+        if(queue.size() > 1){
+            queue.remove(index);
+            queue.add(index, queue.size() - 1);
+            queue.remove(queue.size() - 1);
+            bubbleUpOrDown(index);
+        }
+    }
+
+    void bubbleUp(int index){
+        int position = index;
+        while(position != 0){
+            E upElement = queue.get(position);
+        }
+    }
+
+    void bubbleDown(int index){
+
+    }
+
+    void bubbleUpOrDown(int index){
 
     }
 }
